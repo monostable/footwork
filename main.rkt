ab7cc0aa10b870652a13d53c8af01d9334d36e60
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-05-18 19:05:21 +0100

    Add file menu and make top canvas no-focus

--- main.rkt
+++ main.rkt
@@ -3,6 +3,7 @@
 
 (define frame (new frame% [label "Footwork"]))
 (define menu-bar (new menu-bar% [parent frame]))
+(define menu-option-file (new menu% [label "File"] [parent menu-bar]))
 (define menu-option-edit (new menu% [label "Edit"] [parent menu-bar]))
 (append-editor-operation-menu-items menu-option-edit #f)
 (define our-canvas%
@@ -17,7 +18,12 @@
   (class editor-canvas%
     (super-new)))
 
-(define canvas (new our-canvas% [parent frame]))
+(define canvas
+  (new our-canvas%
+       [parent frame]
+       [style (list 'no-focus)]))
+
+(send canvas accept-tab-focus #f)
 (define editor-canvas (new our-editor-canvas% [parent frame]))
 (define text (new text%))
 
