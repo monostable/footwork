401dd32ddce493399f8104e033c359020f76566c
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-05-18 18:12:48 +0100

    Steal key events from the editor canvas

--- main.rkt
+++ main.rkt
@@ -6,9 +6,18 @@
 (define menu-option-edit (new menu% [label "Edit"] [parent menu-bar]))
 (append-editor-operation-menu-items menu-option-edit #f)
 
-(define canvas (new editor-canvas% [parent frame]))
+(define our-editor-canvas%
+  (class editor-canvas%
+    (define/override (on-char ke)
+      (match (send ke get-key-code)
+             [x
+               (eprintf "got ~v\n" x)]))
+    (super-new)))
+
+(define canvas (new our-editor-canvas% [parent frame]))
 (define text (new text%))
 
 (send canvas set-editor text)
-(send frame show #t)
 (send text load-file "example.kicad_mod")
+(eprintf "~a" (send text get-text))
+(send frame show #t)
