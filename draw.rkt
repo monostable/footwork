8fda10976111c3e2530e202e84bf45bc2b5c811c
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-05-20 22:04:08 +0100

    Make it concurrent!

--- draw.rkt
+++ draw.rkt
@@ -4,17 +4,29 @@
 ;  (cons x y)
 ;  )
 
+(define (apply-function-list flist element)
+  (for-each (lambda (f)
+         (thread (lambda () ((f element)))))
+       flist))
+
 (define-syntax-rule
   (fp_text str (at x y))
-  (lambda (canvas dc)
+  (lambda (dc)
     (send dc set-scale 1 1)
     (send dc set-text-foreground "blue")
     (send dc draw-text str x y)))
+
+(define-syntax-rule
+  (module name (layer front-layer) (tedit time) items)
+  (lambda (dc) (apply-function-list items dc)))
+
+(define my-module
+  (module "" (layer "") (tedit "") (list (fp_text "hi" (at 1 1)))))
 
 (define frame (new frame%
                    [label "Example"]
                    [width 300]
                    [height 300]))
 (new canvas% [parent frame]
-             [paint-callback (fp_text "hi" (at 1 1))])
+             [paint-callback (lambda (canvas dc) (my-module dc))])
 (send frame show #t)
