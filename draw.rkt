dea42854b9abd693e4a5cb0ce1196232f31bf537
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-05-21 11:40:35 +0100

    Work on fp_text

--- draw.rkt
+++ draw.rkt
@@ -1,25 +1,30 @@
 #lang racket/gui
 
+(define sides '(top bottom))
+(define layers '(F.Cu B.Cu))
+
 (define (execute-functions flist . args)
   (for-each
-   (lambda (function)
+   (lambda (function!)
      [thread (lambda ()
-        [apply function args])])
+        [apply function! args])])
    flist))
 
 (define-syntax-rule
   (fp_text str (at x y))
-  (lambda (dc)
+  (lambda (side dc)
     (send dc set-scale 1 1)
-    (send dc set-text-foreground "blue")
+    (if [eq? side 'top]
+      (send dc set-text-foreground "blue")
+      (send dc set-text-foreground "red"))
     (send dc draw-text str x y)))
 
 (define-syntax-rule
-  (module name (layer l) (tedit t) items ...)
-  (lambda (dc) (execute-functions (list items ...) dc)))
+  (module name (layer lay) (tedit t) items ...)
+  (lambda (dc) (execute-functions (list items ...) (if [eq? lay 'F.Cu] 'top 'bottom) dc)))
 
 (define my-module
-  (module Neosid_Air-Coil_SML_1turn_HDM0131A (layer F.Cu) (tedit 56CA2F43)
+  (module Neosid_Air-Coil_SML_1turn_HDM0131A (layer 'F.Cu) (tedit 56CA2F43)
     (fp_text "hi" (at 1 1)) (fp_text "lo" (at 20 20))))
 
 (define frame (new frame%
