8ee989c4a2c542e6e41c4cde3bda9bce2bd656a5
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-05-21 18:58:02 +0100

    Add module and drawing

--- kicad_mod.rkt
+++ kicad_mod.rkt
@@ -13,9 +13,30 @@
                            "Expected (define-symbols <identifier> ...)"
                            stx)]))
 
+(define (execute-functions flist . args)
+  (for-each
+   (lambda (function!)
+     [thread (lambda ()
+        [apply function! args])])
+   flist))
+
+(define-syntax-rule
+  (fp_text str (at x y))
+  (lambda (side dc)
+    (send dc set-scale 1 1)
+    (if [eq? side 'top]
+      (send dc set-text-foreground "blue")
+      (send dc set-text-foreground "red"))
+    (send dc draw-text str x y)))
+
 (provide (except-out (all-from-out racket) #%module-begin)
          (rename-out [module-begin #%module-begin]))
 
+(define-syntax-rule
+  (kicad_module name (layer _layer) (tedit t) items ...)
+  (lambda (dc) (execute-functions (list items ...) (if [eq? _layer 'F.Cu] 'top 'bottom) dc)))
+
 (define-syntax-rule (module-begin expr ...)
   (#%module-begin
-   (provide-symbols F.Cu B.Cu)))
+   (provide-symbols F.Cu B.Cu)
+   (provide fp_text kicad_module)))
--- test.rkt
+++ test.rkt
@@ -1,4 +1,15 @@
-#lang racket
+#lang racket/gui
 (module kicad_mod "kicad_mod.rkt")
 (require 'kicad_mod)
-(println B.Cu)
+
+(define my-module
+  (kicad_module Neosid_Air-Coil_SML_1turn_HDM0131A (layer B.Cu) (tedit 56CA2F43)
+    (fp_text "hi" (at 1 1)) (fp_text "lo" (at 20 20))))
+
+(define frame (new frame%
+                   [label "Example"]
+                   [width 300]
+                   [height 300]))
+(new canvas% [parent frame]
+             [paint-callback (lambda (canvas dc) (my-module dc))])
+(send frame show #t)
