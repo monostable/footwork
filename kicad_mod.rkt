b7c511b6372f3efe8c408fe71b91de916806df32
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-10-21 22:50:14 +0100

    Remove typedness from kicad_mod for now

--- kicad_mod.rkt
+++ kicad_mod.rkt
@@ -1,22 +1,23 @@
-#lang typed/racket/gui
+#lang racket/gui
 
 (provide eval-kicad_mod/draw)
 
-(require/typed
- racket/sandbox
- [make-evaluator
-  (->*
-   (Symbol)
-   (#:requires (List String))
-   (-> String (String -> Any)))])
+(require racket/sandbox)
+;(require
+; racket/sandbox
+; [make-evaluator
+;  (->*
+;   (Symbol)
+;   (#:requires (List String))
+;   (-> String (String -> Any)))])
 
-(: make-kicad-mod-evaluator
-   (-> String (-> String (-> String Any))))
+;(: make-kicad-mod-evaluator
+;   (-> String (-> String (-> String Any))))
 (define (make-kicad-mod-evaluator name)
   (make-evaluator
     'racket/base
     #:requires `(,(~a "kicad_mod/" name ".rkt"))))
 
-(: eval-kicad_mod/draw (-> String (String -> Any)))
+;(: eval-kicad_mod/draw (-> String (String -> Any)))
 (define eval-kicad_mod/draw
   (make-kicad-mod-evaluator "draw"))
