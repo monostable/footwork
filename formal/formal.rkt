6507e58489bcafafb4ccc25686895335b1e924d6
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-11-07 00:12:23 +0000

    Come up with better formal function macro

--- formal/formal.rkt
+++ formal/formal.rkt
@@ -1,12 +1,18 @@
 #lang racket
 
 (define-syntax-rule (define-formal f)
-  (define-syntax-rule (f arguments (... ...))
-     `(begin (define f #t) (f ,arguments (... ...)))))
+  (define f
+    (lambda arguments
+      (quasiquote (f (unquote-splicing arguments))))))
 
 (define-formal f)
 
-(f 1)
-; => '(f 1 2 3)
+(define-formal g)
 
-(map f '(1 2 3))
+(f (g 1))
+
+(f)
+
+(map f '(1 2 (g 3)))
+
+(foldl f 'x0 '(a b c))
