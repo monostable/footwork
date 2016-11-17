f674350a2169573062b3a1a30010c60d0e212c7b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-11-17 19:58:39 +0000

    Try and define multi-arity `at` expression

--- kicad_mod/syntax.rkt
+++ kicad_mod/syntax.rkt
@@ -19,10 +19,12 @@
   (fp_text str (at x y))
   (kicad `(fp_text ,str (at ,x ,y))))
 
-(pretty-print
-  (kicad-expand
-    (for/list ([i (range 5)])
-      (for/list ([j (range 5)] [k (range 3)])
-        (list
-          (fp_text 1 (at 0 i))
-          (fp_line (start 0 0) (end i j) (layer F.Cu) (width k)))))))
+;(pretty-print
+;  (kicad-expand
+;    (for/list ([i (range 5)])
+;      (for/list ([j (range 5)] [k (range 3)])
+;        (list
+;          (fp_text 1 (at 0 i))
+;          (fp_line (start 0 0) (end i j) (layer F.Cu) (width k))))))
+;
+(provide kicad-expand)
--- kicad_mod/typed.rkt
+++ kicad_mod/typed.rkt
@@ -2,13 +2,34 @@
 (require/typed racket/undefined
                [undefined Any])
 
-(require "formal.rkt")
+(struct kicad ([expr : (Listof Any)]))
+
+(define (kicad->list expr)
+  (match expr
+    [(kicad (list e ...)) (quasiquote ((unquote-splicing e)))]))
+
+(: kicad-expand (-> (Listof Any) (Listof Any)))
+(define (kicad-expand e)
+  (map kicad->list
+    (flatten e)))
+
+
+(define-syntax-rule (define-typed-kicad f argument-types ...)
+  (begin
+    (: f (argument-types ... -> kicad))
+    (define f (lambda arguments (kicad (quasiquote (f (unquote-splicing arguments))))))))
 
 (define-syntax-rule (define-typed f argument-types ...)
   (begin
     (: f (argument-types ... -> (Listof Any)))
     (define f (lambda arguments (quasiquote (f (unquote-splicing arguments)))))))
 
-(define-typed fp_line String String)
+(define-syntax-rule (define-formal f)
+  (define f (lambda arguments (quasiquote (f (unquote-splicing arguments))))))
 
-(fp_line "" "")
+(define-typed-kicad fp_line String (Listof Any))
+
+(: at (->* (Number Number) (Number) (Listof Any)))
+(define-formal at)
+
+(kicad-expand (for/list ([i (range 5)]) (fp_line "" (at i 2.0 5))))
