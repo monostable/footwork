4b09bdb06ba675c4d35930e05b86532ee76cbdde
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-11-17 21:13:08 +0000

    Play around with `at` type

--- kicad_mod/typed.rkt
+++ kicad_mod/typed.rkt
@@ -6,9 +6,9 @@
 
 (define (kicad->list expr)
   (match expr
-    [(kicad (list e ...)) (quasiquote ((unquote-splicing e)))]))
+    [(kicad (list e ...)) `(,@e)]))
 
-(: kicad-expand (-> (Listof Any) (Listof Any)))
+(: kicad-expand ((Listof Any) -> (Listof Any)))
 (define (kicad-expand e)
   (map kicad->list
     (flatten e)))
@@ -17,19 +17,23 @@
 (define-syntax-rule (define-typed-kicad f argument-types ...)
   (begin
     (: f (argument-types ... -> kicad))
-    (define f (lambda arguments (kicad (quasiquote (f (unquote-splicing arguments))))))))
+    (define f (lambda arguments (kicad `(f ,@arguments))))))
 
-(define-syntax-rule (define-typed f argument-types ...)
-  (begin
-    (: f (argument-types ... -> (Listof Any)))
-    (define f (lambda arguments (quasiquote (f (unquote-splicing arguments)))))))
+(define-syntax define-typed
+  (syntax-rules ()
+    [(define-typed f (argument-types ...) (optional-types ...))
+     (begin
+       (: f (->* (argument-types ...) (optional-types ...) (Listof Any)))
+       (define f (lambda arguments `(f ,@arguments))))]
+    [(define-typed f (argument-types ...))
+     (define-typed f (argument-types ...) ())]))
 
 (define-syntax-rule (define-formal f)
-  (define f (lambda arguments (quasiquote (f (unquote-splicing arguments))))))
+  (define f (lambda arguments `(f ,@arguments))))
+
 
 (define-typed-kicad fp_line String (Listof Any))
 
-(: at (->* (Number Number) (Number) (Listof Any)))
-(define-formal at)
+(define-typed at (Number Number) (Number))
 
-(kicad-expand (for/list ([i (range 5)]) (fp_line "" (at i 2.0 5))))
+(kicad-expand (for/list ([i (range 5)]) (fp_line "" (at i 2.0))))
