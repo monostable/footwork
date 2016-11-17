f143d49bd8e81aeae72b9abd6ba568beb4e6b26c
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-11-17 22:56:03 +0000

    Work out optional arguments with syntax-parse

--- kicad_mod/typed.rkt
+++ kicad_mod/typed.rkt
@@ -2,6 +2,8 @@
 (require/typed racket/undefined
                [undefined Any])
 
+(require (for-syntax syntax/parse))
+
 (struct kicad ([expr : (Listof Any)]))
 
 (define (kicad->list expr)
@@ -28,20 +30,17 @@
     [(define-typed f (argument-types ...))
      (define-typed f (argument-types ...) ())]))
 
-(define-syntax-rule (define-formal f)
-  (define f (lambda arguments `(f ,@arguments))))
-
 (define-typed at (Number Number) (Number))
 
+(: unless-equals (Any Any -> (Listof Any)))
+(define (unless-equals test value)
+  (filter (lambda (x) (not (eq? (void) x))) (list (unless (eq? test value) value))))
 
-(define-syntax fp_text
-  (syntax-rules ()
-    [(fp_text str (at x y))
-     (kicad `(fp_text ,str (at ,x ,y)))]
-    [(fp_text str (at x y o))
-     (kicad `(fp_text ,str (at ,x ,y ,o)))]))
-
+(define-syntax (fp_text stx)
+  (syntax-parse stx
+    [(fp_text str (at x y (~optional o #:defaults ([o #'0]))))
+     #'(kicad `(fp_text ,str (at ,x ,y ,@(unless-equals 0 o))))]))
 
 (pretty-print
   (kicad-expand
-    (for/list ([i (range 5)]) (fp_text "" (at i 2.0 180)))))
+    (for/list ([i (range 5)]) (fp_text "" (at 0 2.0 i)))))
