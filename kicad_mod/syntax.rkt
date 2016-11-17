096e28bc0b59e38bb6e09a7d2fd49e484dfa0f5b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-11-17 16:38:31 +0000

    Add fp_text to syntax

--- kicad_mod/syntax.rkt
+++ kicad_mod/syntax.rkt
@@ -7,19 +7,22 @@
   (match expr
     [(kicad (list e ...)) (quasiquote ((unquote-splicing e)))]))
 
+(define (kicad-expand e)
+  (map kicad->list
+    (flatten e)))
+
 (define-syntax-rule
   (fp_line (start start-x start-y) (end end-x end-y) (layer l) (width w))
   (kicad `(fp_line (start ,start-x ,start-y) (end ,end-x ,end-y) (layer ,l) (width ,w))))
 
-;(let ([pattern (fp_line (start start-x start-y) (end end-x end-y) (layer l) (width w))])
-;  (define-syntax-rule
-;    pattern
-;    `(fp_line (start #,start-x #,start-y) (end #,end-x #,end-y) (layer #,l) (width #,w)))
+(define-syntax-rule
+  (fp_text str (at x y))
+  (kicad `(fp_text ,str (at ,x ,y))))
 
-
-
-(map kicad->list
-  (flatten
+(pretty-print
+  (kicad-expand
     (for/list ([i (range 5)])
       (for/list ([j (range 5)] [k (range 3)])
-        (fp_line (start 0 0) (end i j) (layer F.Cu) (width k))))))
+        (list
+          (fp_text 1 (at 0 i))
+          (fp_line (start 0 0) (end i j) (layer F.Cu) (width k)))))))
